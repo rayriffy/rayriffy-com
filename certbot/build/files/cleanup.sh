cb7c64c54ce56141fc116838d214d47e9accfe5e
Phumrapee Limpianchop <git@rayriffy.com>
2018-09-10 16:52:44 +0700

    :sparkles: Change certbot to build locally | :hankey: Testing on 1 container

---
+++ certbot/build/files/cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e
+
+lexicon $LEXICON_OPTIONS $LEXICON_PROVIDER $LEXICON_PROVIDER_OPTIONS delete $CERTBOT_DOMAIN TXT --name="_acme-challenge.$CERTBOT_DOMAIN." --content="$CERTBOT_VALIDATION"
